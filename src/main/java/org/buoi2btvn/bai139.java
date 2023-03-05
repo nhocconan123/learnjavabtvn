422f9803c91db53a0782fc1dfdac8f8fbd76dc99
Huy Duong <nhocconan008@gmail.com>
2023-03-05 22:57:13 +0700

    btvn buoi2 java1

--- src/main/java/org/buoi2btvn/bai139.java
+++ src/main/java/org/buoi2btvn/bai139.java
@@ -2,17 +2,28 @@ package org.buoi2btvn;
 
 public class bai139 {
     public static void main(String[] args) {
-        int arr[] = {2200, -2555, -367, 600, -997};
-        int sum = 0;
-        for( int i = 0; i < arr.length; i++){
-            if(arr[i] % i == 0){
-                sum += i;
-            }
-        }
-        if(sum == arr.length){
-            for(int i = arr.length - 1; i >= 0; i--){
-
-            }
+        int arr[] = {28, 4, -367, 600, -997};
+        for (int i=0;i<arr.length;i++)
+        {
+            if(sohoanthien(arr[i]));
+            System.out.println("so"+arr[i]+"la so hoan thien");
         }
     }
+    public static boolean sohoanthien(int x)
+    {
+        int tam=0;
+        for(int i=1;i<x;i++)
+        {
+            if(x%i==0)
+            {
+                tam=tam+i;
+            }
+        }
+        if(tam!=0 && tam==x)
+        {
+            return true;
+        }else
+            return false;
+    }
 }
+
